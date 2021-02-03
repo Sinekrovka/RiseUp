c4c9a7fbd96b36a4d5c3479387ebf3fea52fa3ea
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2021-02-04 00:34:24 +0600

    Add Enemy

--- Assets/Scripts/StickMove.cs
+++ Assets/Scripts/StickMove.cs
@@ -17,12 +17,12 @@ public class StickMove : MonoBehaviour
         Vibration.Init();
     }
 
-    /*private void OnMouseDrag()
+    private void OnMouseDrag()
     {
         mouseDrag.x = cam.ScreenToWorldPoint(Input.mousePosition).x;
         mouseDrag.y = cam.ScreenToWorldPoint(Input.mousePosition).y;
         transform.position = mouseDrag;
-    }*/
+    }/**/
     /*Для отладки на пк*/
 
     private void FixedUpdate()
